a05b00b84a558ac9b0f57ccba322407ae4598f50
afeiship <1290657123@qq.com>
2020-12-24 15:32:47 +0800

    refactor: add thanks to

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,16 @@
+/*!
+ * name: @jswork/next-url-status
+ * description: Detect url status code for next.
+ * homepage: https://github.com/afeiship/next-url-status
+ * version: 1.0.0
+ * date: 2020-12-24 15:32:14
+ * license: MIT
+ */
+
 (function () {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('@jswork/next');
   var HEADERS = { 'User-Agent': 'node/http', 'Accept': '*/*' };
-
-  //@thanks to: https://github.com/zrrrzzt/url-status-code/blob/master/lib/check-url-status.js
 
   nx.urlStatus = function (inUrl) {
     var protocol = /https/.test(inUrl) ? 'https' : 'http';
